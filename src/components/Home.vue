ca09e94f826c2d790c3a0d770645893d03092007
Wqm <577171673@qq.com>
2019-12-30 14:13:00 +0800

    Page built

---
+++ src/components/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home-container">
+    <Navigation></Navigation>
+    <div>
+      <div>
+        <Sidebar></Sidebar>
+      </div>
+      <div>
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+    import Navigation from "../components/Navigation";
+    import Sidebar from "../components/Sidebar";
+
+    export default {
+      name: "Home",
+      components: {Sidebar, Navigation}
+    }
+</script>
+
+<style scoped>
+  .home-container{
+    height: 100%;
+    width: 100%;
+  }
+</style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+    <el-menu
+      :default-active="activeIndex"
+      class="el-menu-demo"
+      mode="horizontal"
+      @select="handleSelect"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b">
+      <el-menu-item index="1">学生选课成绩管理系统</el-menu-item>
+      <el-menu-item index="2" style="float: right">注销</el-menu-item>
+      <el-menu-item index="3" style="float: right">Wqm</el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navigation',
+  data () {
+    return {
+      activeIndex: '',
+      stuName: 'Wqm'
+    }
+  },
+  methods:{
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .container {
+    height: auto;
+  }
+</style>
