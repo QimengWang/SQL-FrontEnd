042a800a282357e7118585b9738b103a13f5ac55
Wqm <577171673@qq.com>
2020-01-08 22:22:12 +0800

    add some icons

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -5,22 +5,38 @@
         class="el-menu-vertical-demo"
         @open="handleOpen"
         @close="handleClose"
-        background-color="#545c64"
+        background-color="#303133"
         text-color="#fff"
         active-text-color="#ffd04b">
           <router-link to="/PersonalInformation">
             <el-menu-item index="1">
-              <span slot="title" style="float: left;">个人信息</span>
+              <div>
+                <img src="../assets/user.png" class="image" />
+                <span style="float: left; margin-left: 2%">
+                  个人信息
+                </span>
+              </div>
             </el-menu-item>
           </router-link>
           <router-link to="/SelectCourse">
             <el-menu-item index="2">
-              <span slot="title" style="float: left;">选课</span>
+              <div>
+                <img src="../assets/select.png" class="image"/>
+                <span style="float: left; margin-left: 2%">
+                  选课
+                </span>
+              </div>
             </el-menu-item>
           </router-link>
           <router-link to="/Transcript">
             <el-menu-item index="3">
-              <span slot="title" style="float: left;">成绩单</span>
+              <div>
+                <img src="../assets/transcript.png" class="image" />
+                <span style="float: left; margin-left: 2%">
+                成绩单
+              </span>
+              </div>
+
             </el-menu-item>
           </router-link>
       </el-menu>
@@ -55,5 +71,12 @@
 
   .el-menu-vertical-demo {
     height: 100%;
+  }
+
+  .image{
+    margin-top: 12%;
+    height: 10%;
+    width: 10%;
+    float: left;
   }
 </style>
