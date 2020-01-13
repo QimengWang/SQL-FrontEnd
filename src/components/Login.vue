d6a8965f614381bd64bf433b431f68808150438d
Wqm <577171673@qq.com>
2020-01-13 22:39:36 +0800

    Minor change

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,23 +1,27 @@
 <template>
   <div class="container">
-    <div style="margin-top: 9%; margin-left: 35%">
+    <div style="margin-top: 8%; margin-left: 35%">
       <el-card class="box-card">
 
         <div style="margin-top: 3%; font-weight: bold; font-size: 23px;">
           <span>登 录</span>
         </div>
 
-        <Tabs value="student" style="margin-top: 8px;" :animated="false">
+        <Tabs value="student" style="margin-top: 10px;" :animated="false">
           <TabPane label="学生" name="student">
             <el-form :model="stu" style="font-weight: bold;">
               <el-form-item label="学号：" style="display: inline">
-                <el-input placeholder="请输入学号" style="width: 300px;" v-model="stu.id"></el-input>
+                <el-input placeholder="请输入学号" style="width: 300px;" v-model="stu.id"
+                          auto-complete="new-password">
+                </el-input>
               </el-form-item>
               <el-form-item label="密码:">
-                <el-input placeholder="请输入密码" style="width: 300px;" v-model="stu.password" show-password></el-input>
+                <el-input placeholder="请输入密码" style="width: 300px;" v-model="stu.password" show-password
+                          auto-complete="new-password">
+                </el-input>
               </el-form-item>
               <el-form-item>
-                <el-button @click="stuLogin" type="info" style="width: 300px; background-color: #545c64; font-weight: bold;">
+                <el-button @click="stuLogin" type="info" class="button">
                   确认
                 </el-button>
               </el-form-item>
@@ -26,13 +30,16 @@
           <TabPane label="教师" name="teacher">
             <el-form :model="tea" style="font-weight: bold;">
               <el-form-item label="工号：" style="display: inline">
-                <el-input placeholder="请输入工号" style="width: 300px;" v-model="tea.id"></el-input>
+                <el-input placeholder="请输入工号" style="width: 300px;" v-model="tea.id"
+                          auto-complete="new-password">
+                </el-input>
               </el-form-item>
               <el-form-item label="密码:">
-                <el-input placeholder="请输入密码" style="width: 300px;" v-model="tea.password" show-password></el-input>
+                <el-input placeholder="请输入密码" style="width: 300px;" v-model="tea.password" show-password
+                          auto-complete="new-password"></el-input>
               </el-form-item>
               <el-form-item>
-                <el-button @click="teaLogin" type="info" style="width: 300px; background-color: #545c64; font-weight: bold;">
+                <el-button @click="teaLogin" type="info" class="button">
                   确认
                 </el-button>
               </el-form-item>
@@ -105,5 +112,19 @@
     width: 350px;
     background: rgba(255,255,255,0.7);
     text-align: center;
+  }
+
+  .button {
+    width: 300px;
+    background-color: #545c64;
+    font-weight: bold;
+  }
+
+  >>>.ivu-tabs-nav-wrap {
+    text-align: center;
+  }
+
+  >>>.ivu-tabs-nav-scroll {
+    display: inline-block;
   }
 </style>
