f2ff3041ab3e351c9d8a4ed48cfe957d0e07382a
CL-xkay <2869625763@qq.com>
2020-06-16 00:16:42 +0800

    管理员个人信息显示

--- src/components/mPersonalInformation.vue
+++ src/components/mPersonalInformation.vue
@@ -1,13 +1,73 @@
 <template>
-    <h1>Hello CL!</h1>
+  <div class="container">
+    <el-card>
+      <h1 style="margin-top: 10px">基本信息</h1>
+      <el-form :model="mPersonalInfo" label-width="100px">
+        <el-form-item label="姓名">
+          {{mPersonalInfo.xm}}
+        </el-form-item>
+        <el-form-item label="工号">
+          {{mPersonalInfo.gh}}
+        </el-form-item>
+        <el-form-item label="性别">
+          {{mPersonalInfo.xb}}
+        </el-form-item>
+        <el-form-item label="手机号码">
+          {{mPersonalInfo.sjhm}}
+        </el-form-item>
+        <el-form-item label="邮箱地址">
+          {{mPersonalInfo.yxdz}}
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "mPersonalInformation"
+  import {mPersonalInfo, tPersonalInfo} from "../api/api";
+
+  export default {
+    name: "mPersonalInformation",
+    data() {
+      return {
+        labelPosition: 'right',
+        mPersonalInfo: {},
+      }
+    },
+    methods: {
+      async getData() {
+        this.mPersonalInfo = (await mPersonalInfo()).data;
+      },
+    },
+    async mounted() {
+      this.getData();
     }
+  }
 </script>
 
 <style scoped>
+  .container {
+    height: 100%;
+    width: calc(100% - 200px);
+    /*width: 100%;*/
+    background-color: whitesmoke;
+  }
 
-</style>
+  >>> .el-form {
+    margin-top: 30px;
+    width: 40%;
+    position: relative;
+    left: 50%;
+    transform: translate(-50%, 0%);
+  }
+
+  >>> .el-form-item__label {
+    color: #333333;
+    font-weight: bold;
+  }
+
+  >>> .el-card {
+    margin: 50px 50px 180px;
+    color: #333333;
+  }
+  </style>
